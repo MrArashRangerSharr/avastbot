7c9cfca596bd36c572290e9bbf744c1cf331ac81
unknowntg <arashnomiri@gmail.com>
2016-05-06 18:01:35 +0430

    Update version.lua

--- plugins/version.lua
+++ plugins/version.lua
@@ -14,12 +14,12 @@
 do
 
 function run(msg, matches)
-  return 'DBTeam Bot V1 Supergroups\nAn advanced Administration bot based on yagop/telegram-bot\n\nDevelopers: @MaSkAoS @Josepdal\nSupport: @skneos @iicc1 @Serx666\nChannels: @DBTeamEN @DBTeamES\n\nCheckout: https://github.com/Josepdal/DBTeam\nGNU GPL v2 license.'
+  return 'avast Bot V1 Supergroups/api\nAn advanced Administration bot based on lua via api\n\nDeveloped by: @avast_Team'
 end
 
 return {
   patterns = {
-    "^#version$"
+    "^version$"
   }, 
   run = run 
 }
